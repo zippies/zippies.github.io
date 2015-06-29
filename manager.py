84b1994a9af2215c926acbaa08f775796e3a5ef1
unknown <chenzichaocc@163.com>
2015-06-29 21:38:47 +0800

    add login.html

--- manager.py
+++ manager.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route('/index.html')
 def index():
 	return render_template('index.html')
+
+def login():
+	return render_template('login.html')
 	
+
 if __name__ == '__main__':
 	app.run()
